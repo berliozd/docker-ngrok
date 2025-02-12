6761a31b53993726bfddf0ed7f8fa2c89f85fd27
Didier Berlioz <berliozd@gmail.com>
2025-02-12 12:35:47 +0100

    Update entrypoint.sh
    use domain instead of subdomain

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,7 +43,7 @@ fi
 if [ -n "$NGROK_HOSTNAME" ] && [ -n "$NGROK_AUTH" ]; then
   ARGS="$ARGS -hostname=$NGROK_HOSTNAME "
 elif [ -n "$NGROK_SUBDOMAIN" ] && [ -n "$NGROK_AUTH" ]; then
-  ARGS="$ARGS -subdomain=$NGROK_SUBDOMAIN "
+  ARGS="$ARGS -domain=$NGROK_SUBDOMAIN "
 elif [ -n "$NGROK_HOSTNAME" ] || [ -n "$NGROK_SUBDOMAIN" ]; then
   if [ -z "$NGROK_AUTH" ]; then
     echo "You must specify an authentication token after registering at https://ngrok.com to use custom domains."
